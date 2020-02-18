f6f69cc19ff77f59c1ac277aa2b90afc60d7baf8
Debroneee <bunakov.ua@gmail.com>
2020-02-18 15:47:49 +0300

    add and stylized featured component and buttons

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,15 @@
 import React from "react"
 import Nav from "../components/nav/nav"
+import Featured from "../components/featured/featured"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <Nav/>
-  </Layout>
+    <Layout>
+        <SEO title="Home"/>
+        <Nav/>
+        <Featured/>
+    </Layout>
 )
 
 export default IndexPage
