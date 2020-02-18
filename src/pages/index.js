045e4e489b6ab81fde83ad110f903a751b94f3a7
Debroneee <bunakov.ua@gmail.com>
2020-02-18 18:48:57 +0300

    add viewmore button, hide scrollbar, add footer component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,13 @@
 import React from "react"
+import { Link } from "gatsby"
 import Nav from "../components/nav/nav"
 import Featured from "../components/featured/featured"
 import Home from "../components/home/home"
+import Footer from "../components/footer/footer"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+
+import "./index.css"
 
 const IndexPage = () => (
     <Layout>
@@ -11,6 +15,8 @@ const IndexPage = () => (
         <Nav/>
         <Featured/>
         <Home/>
+        <Link to="/blog" className="viewmore">View More</Link>
+        <Footer/>
     </Layout>
 )
 
