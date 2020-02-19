408056f0154a5f43a55db8452067504b51e0b1a1
bunakov94 <bunakov.code@gmail.com>
2020-02-19 15:16:24 +0300

    favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,12 @@
-//Contentful
 require('dotenv').config({
     path: `.env.${process.env.NODE_ENV}`,
 })
-//
 
 module.exports = {
     siteMetadata: {
-        title: `Gatsby Default Starter`,
-        description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-        author: `@gatsbyjs`,
+        title: `Gatsby Blog Template`,
+        description: `Free course on how to create a blog from scratch using React, Gatsby, Contentful, and Netlify.`,
+        author: `Skillthrive`,
     },
     plugins: [
         `gatsby-plugin-react-helmet`,
@@ -16,7 +14,7 @@ module.exports = {
         {
             resolve: `gatsby-source-contentful`,
             options: {
-                spaceId: `c978x602lrcc`,
+                spaceId: process.env.CONTENTFUL_SPACE_ID,
                 accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
             },
         },
@@ -47,11 +45,11 @@ module.exports = {
                 background_color: `#663399`,
                 theme_color: `#663399`,
                 display: `minimal-ui`,
-                icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+                icon: `src/images/favicon.png`, // This path is relative to the root of the site.
             },
         },
         // this (optional) plugin enables Progressive Web App + Offline functionality
         // To learn more, visit: https://gatsby.dev/offline
-        // `gatsby-plugin-offline`,
+        // 'gatsby-plugin-offline',
     ],
 }
